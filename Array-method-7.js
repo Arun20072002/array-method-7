02054d64dde3def2bd911ff5502ceca0b364a134
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-12-04 11:28:14 +0530

    Array-method-7

---
+++ Array-method-7.js
@@ -0,0 +1,7 @@
+document.write("7.check if the car 'toyota' is present at the index 2 // false"+"<br>")
+document.write("--------------------------------------------------------------"+"<br>")
+
+var cars = [ 'ford', 'toyota', 'bmw', 'tesla' ];
+
+var a=cars.includes("toyota",2)
+document.write(a+"<br>")
